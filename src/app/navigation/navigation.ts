c3cbea2cfc90d49938a6e674158b41d45ecffed2
mbektas7 <mbektas7@gmail.com>
2020-06-30 21:40:31 +0300

    second

---
+++ src/app/navigation/navigation.ts
@@ -0,0 +1,57 @@
+import { FuseNavigation } from '@fuse/types';
+
+export const navigation: FuseNavigation[] = [
+    {
+        id       : 'applications',
+        title    : 'Anasayfa',
+        translate: 'NAV.APPLICATIONS',
+        type     : 'group',
+        children : [
+            {
+                id       : 'sample',
+                title    : 'Sample',
+                translate: 'NAV.SAMPLE.TITLE',
+                type     : 'item',
+                icon     : 'email',
+                url      : '/sample',
+                badge    : {
+                    title    : '25',
+                    translate: 'NAV.SAMPLE.BADGE',
+                    bg       : '#F44336',
+                    fg       : '#FFFFFF'
+                }
+            }
+        ]
+    }
+    ,
+    {
+        id       : 'deneme',
+        title    : 'Deneme',
+        translate: 'NAV.SAMPLE.TITLE',
+        type     : 'group',
+        icon     : 'email',
+        url      : '/deneme',
+        badge    : {
+            title    : '25',
+            translate: 'NAV.SAMPLE.BADGE',
+            bg       : '#F44336',
+            fg       : '#FFFFFF'
+        },
+        children : [
+            {
+                id       : 'sample',
+                title    : 'Sample',
+                translate: 'NAV.SAMPLE.TITLE',
+                type     : 'item',
+                icon     : 'email',
+                url      : '/sample',
+                badge    : {
+                    title    : '25',
+                    translate: 'NAV.SAMPLE.BADGE',
+                    bg       : '#F44336',
+                    fg       : '#FFFFFF'
+                }
+            }
+        ]
+    }
+];
