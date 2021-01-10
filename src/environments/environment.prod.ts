6b466f4f89c28dcb4efc527cc2f1c6dc3d979e08
mbektas7 <mbektas7@gmail.com>
2021-01-10 19:39:52 +0300

    profile_sayfasında_fotograf_ayarlandı

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     hmr       : false,
-    //rootPath:'http://localhost:55555/api/',
+   // rootPath:'http://localhost:55555/api/',
    // apiUrl:'http://localhost:55555/api/'
 
    rootPath:'http://amaroktr.com/api/',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@
 export const environment = {
     production: false,
     hmr       : false,
-    //rootPath:'http://localhost:55555/api/',
+   // rootPath:'http://localhost:55555/api/',
    // apiUrl:'http://localhost:55555/api/'
 
-   rootPath:'http://amaroktr.com/api/',
+  rootPath:'http://amaroktr.com/api/',
    apiUrl:'http://amaroktr.com/api/'
 };
 
