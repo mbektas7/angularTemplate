3cfc159dbf87060ecedbb111ad3f2316f30ec2f1
mbektas7 <mbektas7@gmail.com>
2021-02-17 20:21:59 +0300

    blogg

--- src/environments/environment.hmr.ts
+++ src/environments/environment.hmr.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: false,
     hmr       : true,
-    rootPath:'http://localhost:55555/api/',
-    apiUrl:'http://localhost:55555/api/'
+    //rootPath:'http://localhost:55555/api/',
+   // apiUrl:'http://localhost:55555/api/'
 
-   //rootPath:'http://amaroktr.com/api/',
-   //apiUrl:'http://amaroktr.com/api/'
+   rootPath:'http://amaroktr.com/api/',
+   apiUrl:'http://amaroktr.com/api/'
 };
 
